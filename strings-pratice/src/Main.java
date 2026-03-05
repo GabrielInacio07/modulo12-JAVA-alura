345ddd0eb6aa10df68da77875ead8e8004ad2080
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-03-05 00:08:01 -0300

    Ex03 - Substituição de palavras com validação usando contains() e replace()"

--- strings-pratice/src/Main.java
+++ strings-pratice/src/Main.java
@@ -2,23 +2,30 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Ex02");
+        System.out.println("Ex03");
 
         Scanner input =  new Scanner(System.in);
 
         System.out.print("Digite o texto: ");
         String texto = input.nextLine();
 
-        String maiusculo = texto.toUpperCase();
-        String minusculo = texto.toLowerCase();
+        System.out.println("Texto: " + texto);
 
-        if(texto.trim().isEmpty()){
-            System.out.println("Texto não pode ser vázio");
-        }else{
-            System.out.println("Texto em maiúsculas: " + maiusculo);
-            System.out.println("Texto em minúsculas: " + minusculo);
+        System.out.print("Digite a palavra a ser substituída: ");
+        String substituida = input.nextLine();
+
+        if (substituida.trim().isEmpty()) {
+            System.out.println("Palavra inválida!");
+
+        }else if (texto.toLowerCase().contains(substituida.toLowerCase()))  {
+            System.out.print("Digite a nova palavra: ");
+            String novaPalavra = input.nextLine();
+
+            String textoModificado = texto.replaceAll("(?i)" +substituida, novaPalavra);
+            System.out.println("Texto modificado: " + textoModificado);
+        } else {
+            System.out.println("Palavra não encontrada!");
         }
-
 
         input.close();
     }
