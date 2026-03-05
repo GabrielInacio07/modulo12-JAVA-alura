66c81dfc2a115d3adde2c6d53b366140e2493fb5
Gabriel Inacio <inaciogabriell89@gmail.com>
2026-03-04 23:39:24 -0300

    Preparando ambiente - Pattern/Matcher conceitos regex

--- strings-pratice/src/Main.java
+++ strings-pratice/src/Main.java
@@ -1,6 +1,34 @@
-class Main{
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+//https://regex101.com/
+
+class Main{
     public static void main(String[] args) {
 
+//        String texto = "ADMIN: teste@gmail.com";
+//
+//        Pattern pattern = Pattern.compile("\\w+@\\w+.\\w+");
+//        Matcher matcher = pattern.matcher(texto);
+//
+//        if(matcher.find()){
+//            System.out.println(matcher.group());
+//        }
+
+        System.out.println(telfoneFormatter("18981466635"));
+
+    }
+
+
+    public static String telfoneFormatter(String telefone){
+        String regex = "(\\d{2})(\\d{4,5})(\\d{4})";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(telefone);
+
+        if(matcher.matches()){
+            return String.format("(%s) %s-%s", matcher.group(1),matcher.group(2),matcher.group(3));
+        }
+
+        return "Número de telefone Inválido";
     }
 }
